5c7431276952dea2e2444469c2c5ad8299ceae07
abnormallynormal <yichenxiao08@gmail.com>
2025-07-17 22:49:50 -0400

    parts order backend

--- src/app/partsorders/page.tsx
+++ src/app/partsorders/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Navbar from "@/components/Navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card } from "@/components/ui/card";
@@ -5,7 +6,10 @@ import { PendingDataTable } from "./partsorderscomponents/pending-data-table";
 import { IncomingDataTable } from "./partsorderscomponents/incoming-data-table";
 import { ArrivedDataTable } from "./partsorderscomponents/arrived-data-table";
 import { PartOrder, columns } from "./partsorderscomponents/column";
-import {ArrivedPartOrder, arrivedColumns} from "./partsorderscomponents/arrived-column"
+import {
+  ArrivedPartOrder,
+  arrivedColumns,
+} from "./partsorderscomponents/arrived-column";
 import {
   Table,
   TableBody,
@@ -15,23 +19,31 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-async function getData(): Promise<PartOrder[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      partName: "1x2x1x35 Aluminum C-Channel (6-pack) ",
-      quantity: 5,
-      price: 5,
-      totalPrice: 5,
-      teamsRequesting: "x",
-      link: "https://www.vexrobotics.com/channel.html",
-      partNumber: "339-3",
-    },
-    // ...
-  ];
-}
-export default async function PartsOrders() {
-  const data = await getData();
+import { Button } from "@/components/ui/button";
+const data = [
+  {
+    partName: "1x2x1x35 Aluminum C-Channel (6-pack) ",
+    quantity: 5,
+    price: 5,
+    totalPrice: 5,
+    teamsRequesting: "x",
+    link: "https://www.vexrobotics.com/channel.html",
+    partNumber: "339-3",
+  },
+  // ...
+];
+export default function PartsOrders() {
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target?.result;
+        console.log(text);
+      };
+      reader.readAsText(file);
+    }
+  };
   return (
     <div>
       <Navbar />
@@ -46,7 +58,24 @@ export default async function PartsOrders() {
           </TabsList>
           <TabsContent value="pending">
             <Card className="p-4">
-              <div className="font-semibold text-2xl ml-1">Pending Orders</div>
+              <div className="grid grid-cols-2">
+                <div className="font-semibold text-2xl ml-1">
+                  Pending Orders
+                </div>
+                <div className="justify-self-end">
+                  <Button>
+                    <label>
+                      <input
+                        type="file"
+                        name="hi"
+                        className="hidden"
+                        onChange={(e) => handleUpload(e)}
+                      />
+                      Add Orders (Upload CSV File)
+                    </label>
+                  </Button>
+                </div>
+              </div>
               <PendingDataTable columns={columns} data={data} />
             </Card>
           </TabsContent>
