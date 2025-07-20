04667a04d5df0111f558e5e14f57ca28ac22635e
abnormallynormal <yichenxiao08@gmail.com>
2025-07-20 15:17:31 -0400

    eslint warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-wrapper-object-types": "warn",
+    "prefer-const": "warn",
+    "no-var": "warn"
+  }
+  }
 ];
 
 export default eslintConfig;
